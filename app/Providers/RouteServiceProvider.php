1b784cf0f24f3a4d1c4c5c06e897961efdb541a1
Fabrizio Pulvirenti <fabrizio.pulvirenti.86@gmail.com>
2023-02-14 22:23:47 +0100

    Split route api file in more file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -29,9 +30,17 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
+
+            $filesApi = File::allFiles(base_path('routes/api'));
+
+            foreach($filesApi as $key => $path){
+                $files = pathinfo($path);
+                $allApiFile[] = $files['dirname'].'/'.$files['basename'];
+            }
+
             Route::middleware('api')
                 ->prefix('api')
-                ->group(base_path('routes/api.php'));
+                ->group($allApiFile);
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
